9aaa7c9edf98ac41d5c129e5fc8ba8cae7332292
Punnapob Jindakul <punnapob.jdk@gmail.com>
2023-04-09 03:28:34 +0700

    feat: add send button ui

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -11,18 +11,18 @@ const Message = (props: IMessage) => {
 
     return (
         <div className={`flex flex-col ${isOwner ? "self-end" : ""}`}>
-            <header className="flex gap-2 items-center">
+            <header className={`flex gap-2 items-center ${isOwner ? "flex-row-reverse" : ""}`}>
                 <img
-                    className="ml-2 inline-block h-6 w-6 rounded-full ring-2 ring-white dark:ring-gray-800"
+                    className="ml-2 inline-block h-6 w-6 rounded-full ring-2 ring-white"
                     src={mockImg}
                     alt="Image Description"
                 />
                 <p className="text-base">{author}</p>
             </header>
-            <main className={`mt-2 bg-slate-200 p-2 rounded w-fit ${isOwner ? "bg-blue-600 text-slate-100" : ""}`}>
-                <p className="text-xl">{text}</p>
+            <main className={`mt-2 bg-slate-200 py-3 px-4 rounded w-fit ${isOwner ? "bg-blue-600 text-slate-100" : ""}`}>
+                <p className="text-md">{text}</p>
             </main>
-        </div>
+        </div >
     )
 }
 export default Message;
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -16,7 +16,7 @@ const messages = [
         ...mockMessages,
         id: 2,
         author: 'Jane Doe',
-    }
+    }, mockMessages, mockMessages
 ]
 
 const Chat = () => {
@@ -26,8 +26,8 @@ const Chat = () => {
 
             </nav>
             <main className="flex-grow bg-slate-100 min-w-[300px] max-w-lg rounded m-4 min-h-[80%] flex flex-col gap-2">
-                <h1 className="text-2xl font-bold text-slate-100 bg-blue-600 p-4 rounded-t text-right">Room 1</h1>
-                <div className='flex flex-col p-6'>
+                <h1 className="text-2xl font-bold text-slate-100 bg-blue-600 py-3 px-4 rounded-t text-right">Room 1</h1>
+                <div className='flex flex-col p-6 gap-4 flex-grow overflow-x-auto h-10'>
                     {
                         messages.map((message) => {
                             return <Message
@@ -38,6 +38,12 @@ const Chat = () => {
                             />
                         })
                     }
+                </div>
+                <div className='p-6 flex gap-2'>
+                    <input type="text" className="py-3 px-4 block w-full border-slate-400 rounded-md text-sm focus:border-blue-600 focus:ring-blue-600 " />
+                    <button type="button" className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-600 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+                        Send
+                    </button>
                 </div>
             </main>
         </Background>
